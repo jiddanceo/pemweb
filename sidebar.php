473bb495928dbfd8881cfc44a8d85a12b3c359a7
JiddanCeo <73234903+jiddanceo@users.noreply.github.com>
2021-04-19 23:41:18 +0700

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,13 @@
+
+<div class="col-md-4">
+	<!-------awal sidebar----------->
+<ul class="list-group">
+  <li class="list-group-item active" aria-current="true">Produk</li>
+  <li class="list-group-item">Pulsa</li>
+  <li class="list-group-item">Kuota</li>
+  <li class="list-group-item">Voucher</li>
+  <li class="list-group-item">Token PLN</li>
+</ul>
+    <!-------akhir sidebar----------->
+</div>
+    
